f60bab3e7e6772769ebf5fd4679d273e9d060c92
Lea Haselsteiner <leahaselsteiner@hotmail.com>
2025-03-15 12:49:39 +0100

    customer ts

---
+++ ecommerce-client/src/models/Customer.ts
@@ -0,0 +1,13 @@
+export interface ICustomer {
+    id: string; 
+    firstname: string;
+    lastname: string;
+    email: string;
+    phone: string; 
+    address_adress: string;
+    postal_code: number;
+    city: string;
+    country: string;
+    created_at: string;
+  }
+  
