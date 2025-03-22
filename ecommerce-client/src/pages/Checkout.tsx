461b0fbb6e78849576cb3aedc65abd4deecda883
Lea Haselsteiner <leahaselsteiner@hotmail.com>
2025-03-22 08:49:33 +0100

    usestate customer och form

--- ecommerce-client/src/pages/Checkout.tsx
+++ ecommerce-client/src/pages/Checkout.tsx
@@ -1,8 +1,21 @@
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { useCart } from "../Context/CartContext";
 
 export const Checkout = () => {
   const { cart } = useCart(); 
+  const [customer, setCustomer] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    phone: "",
+    street_address: "",
+    postal_code: "",
+    city: "",
+    country: ""
+  });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCustomer({...customer, [e.target.name]: e.target.value})
+  }
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
@@ -18,7 +31,7 @@ export const Checkout = () => {
         headers: {
           "Content-type": "application/json",
         },
-        body: JSON.stringify({ cart }), 
+        body: JSON.stringify({ cart, customer }), 
       });
 
       const data = await response.json();
@@ -42,6 +55,60 @@ export const Checkout = () => {
       </ul>
 
       <form onSubmit={handleSubmit}>
+         <input 
+            name="firstname"  
+            value={customer.firstname}    
+            onChange={handleChange} 
+            placeholder="First name" 
+            required 
+            />
+
+        <input 
+            name="lastname"    
+            value={customer.lastname} 
+            onChange={handleChange} 
+            placeholder="Last name" 
+            required 
+            />
+
+        <input 
+            name="email" 
+            value={customer.email} 
+            onChange={handleChange} 
+            placeholder="Email" 
+            required 
+            />
+        <input 
+            name="phone" 
+            value={customer.phone} 
+            onChange={handleChange} 
+            placeholder="Phone" 
+            />
+
+        <input 
+            name="street_address" 
+            value={customer.street_address} 
+            onChange={handleChange} 
+            placeholder="Address" 
+            />
+        <input 
+            name="postal_code" 
+            value={customer.postal_code} 
+            onChange={handleChange} 
+            placeholder="Postal code" 
+            />
+        <input 
+            name="city" 
+            value={customer.city} 
+            onChange={handleChange} 
+            placeholder="City" 
+            />
+        <input 
+            name="country" 
+            value={customer.country} 
+            onChange={handleChange} 
+            placeholder="Country" 
+            />
         <h3>payment</h3>
         <p>...</p>
         <button type="submit">to checkout</button>
