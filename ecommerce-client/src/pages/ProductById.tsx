7cd442d04bda99a6d93c8fb4770ab62a6384356a
Lea Haselsteiner <leahaselsteiner@hotmail.com>
2025-03-24 09:06:43 +0100

    to be able to add to cart from product site

--- ecommerce-client/src/pages/ProductById.tsx
+++ ecommerce-client/src/pages/ProductById.tsx
@@ -1,12 +1,16 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { fetchProductById } from "../services/productService";
-import { IProduct } from "../models/Product";
+import { IProduct, Product } from "../models/Product";
+import { toast } from "react-toastify";
+import { useCart } from "../Context/CartContext";
+import { CartItem } from "../models/CartItem";
+import { CartActionType } from "../reducers/CartReducer";
 
 export const ProductById = () => {
   const { id } = useParams();
   const [product, setProduct] = useState<IProduct | null>(null);
-
+  const { dispatch } = useCart();
   
   useEffect(() => {
     if (!id) return;
@@ -25,6 +29,13 @@ export const ProductById = () => {
 
   if (!product) return <p>Loading</p>;
 
+const handleAddToCart = (product: Product) => {
+    dispatch({
+      type: CartActionType.ADD_ITEM,
+      payload: new CartItem(product, 1),
+    });
+    toast.success(`One ${product.name} has been added to the cart!`);
+  }; 
   return (
     <div>
       <h2>{product.name}</h2>
@@ -32,6 +43,7 @@ export const ProductById = () => {
       <p>Pris: {product.price}€</p>
       <p>{product.description}</p>
       <p>productID: {product.id}</p>
+      <button onClick={() => handleAddToCart(product)}> add to cart</button>
     </div>
   );
 };
